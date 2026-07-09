eb06ef6a0bfabc5f13d80a5505a86d2cff9014db
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-09 15:33:37 +1000

    create code for task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the single digit numbers using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
