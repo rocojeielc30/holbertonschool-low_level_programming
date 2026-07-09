33a2c69a2c62616caeff68b934f212beee1623f1
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-09 15:22:34 +1000

    create code for task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int x;
+
+	x = 0;
+
+	while (x < 10)
+	{
+		printf("%d\n", x);
+		x++;
+	}
+
+	return (0);
+}
