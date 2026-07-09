73fac9162aed0c389bfe8c52b30c83d86f3d8988
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-09 14:26:12 +1000

    fixed syntax error for checker

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,14 +17,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
-	if (last_digit < 5)
+	if (last_digit > 5)
 	printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 
 	if (last_digit == 0)
 	printf("Last digit of %d is %d and is 0\n", n, last_digit);
 
-	if (last_digit != 0 && last_digit < 6)
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	if (last_digit < 6 && last_digit != 0)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n",
+	n, last_digit);
 
 	return (0);
 }
