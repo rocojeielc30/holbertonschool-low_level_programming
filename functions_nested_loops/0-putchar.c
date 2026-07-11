f13ff4a39f0403b666916861f3099eb020e588a8
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-11 16:54:20 +1000

    code for task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	printf("_putchar,\n");
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+## This directory was created for the Functions, nested loops
+
+All the code in this directory is to satisfy the tasks required 
