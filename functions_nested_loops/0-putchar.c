9c4f1759168f934112de351bc2d31d0a126471d0
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-11 16:59:43 +1000

    code for task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - program that prints putchar
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-
 int main(void)
-
 {
-
-	printf("_putchar,\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
