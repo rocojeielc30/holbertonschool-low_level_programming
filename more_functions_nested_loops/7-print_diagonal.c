053f1e53300b345be81a4dbddaf700e6dca02a1d
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-12 23:08:52 +1000

    create code for task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: number of times to print \
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int spaces;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (spaces = 0; spaces < i; spaces++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
