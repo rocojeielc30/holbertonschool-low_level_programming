1ed034476c8f5e7bc35a10d9bab9fe293ea74263
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-17 15:51:36 +1000

    create a function that copies the elements in a string to another string buffer location

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * *_strcpy - prints n integers in an array that is n big
+ * @dest: pointer to the destination array
+ * @src: pointer to the source array
+ *
+ * Return: dest array pointer.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n;
+	char last;
+
+	n = 0;
+	while (src[n] != '\0')
+	{
+		dest[n] = src[n];
+		n++;
+	}
+	dest[n] = src[n];
+	return (dest);
+}
