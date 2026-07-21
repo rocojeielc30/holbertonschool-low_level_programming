2f6de2f108d713ad81ab3da7b968470aaded40f2
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-21 12:33:47 +1000

    create an array of chars that initializes it with a specific char

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,34 +1,35 @@
 #include <stdlib.h>
 #include "main.h"
 
-char *create_array(unsigned int size, char c) 
-// size is how many characters to allocate 
-// c is the character to fill the array with 
+/**
+ * create_array - creates an array of chars and initializes it with a char
+ * @size: size of the array to create
+ * @c: character to initialize the array with
+ *
+ * Return: pointer to the created array, or NULL if size is 0 or malloc fails
+ */
+
+char *create_array(unsigned int size, char c)
 {
-	char *array; 
-	// pointer that points to the memory 
-	// allocated by malloc
+	char *array;
 	unsigned int i;
 
-	if (size == 0) // exercise is asking to return NULL if size = 0
+	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 
-	// size multiplied by 1 byte (size of a char)
-	array = malloc(size * sizeof(char)); 
+	array = malloc(size * sizeof(char));
 
-	// prints if malloc has failed - ALWAYS CHECK THIS
 	if (array == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
-	// fills the array with the char chosen 
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
 
-	return array;
+	return (array);
 }
