f92ff5c176f7725acc54465a6ccc7d6ad4533204
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-21 23:58:29 +1000

    Create a function that frees a grid of its elements

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid- Write a function that frees a grid of its elements.
+ * @**grid: pointer to a pointer to a grid 
+ * @height: height - how big the array is
+ *
+ * Return: pointer to the created array, or NULL if size is 0 or malloc fails
+ */
+void free_grid(int **grid, int height)
+{
+	int length;
+
+	length = 0;
+	if (grid == NULL)
+	{
+		return;
+	}
+	while (length < height)
+	{
+		free(grid[length]);
+		length++;
+	}
+
+	free(grid);
+
+}
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -13,5 +13,4 @@ Function 4:
 Write a function that returns a pointer to a 2 dimensional array of integers.
 
 Function 5: 
-Write a function that returns a pointer to a 2 dimensional array of integers.
-
+Write a function that frees a 2 dimensional grid previously created by your alloc_grid function.
