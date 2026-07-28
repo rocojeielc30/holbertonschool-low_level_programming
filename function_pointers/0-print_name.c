e0f70840666daa9b1df2083d09e035c6fcb475af
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-28 11:06:57 +1000

    create a function that prints a name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function pointer
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+
+	f(name);
+
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,3 @@
+#This directory was created to store the functions that I will be creating for "C - Function pointers" 
+
+Function 1: 
