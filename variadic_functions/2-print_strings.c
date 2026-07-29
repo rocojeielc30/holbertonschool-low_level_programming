7363d6e3a6f9849e9a6ef57459710d4e3608f29f
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-29 13:50:42 +1000

    create third function and add betty description

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,30 +1,40 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * print_strings - Prints strings followed by a separator.
+ * @separator: String to be printed between each string, or NULL.
+ * @n: Number of strings passed to the function.
+ *
+ * Return: Nothing.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	char string_to_print;
+	char *string_to_print;
 
 	va_start(args, n);
 
 	i = 0;
 	while (i < n)
 	{
-		string_to_print = va_arg(args, char);
+		string_to_print = va_arg(args, char *);
 
 		if (string_to_print == NULL)
 		{
 			printf("nil");
 		}
-
-		printf("%s", string_to_print);
+		else
+		{
+			printf("%s", string_to_print);
+		}
 
 		if (separator != NULL && i < (n - 1))
 		{
 			printf("%s", separator);
 		}
+		i++;
 	}
 
 	printf("\n");
