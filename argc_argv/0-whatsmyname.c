920ce741aa1f8762a099cb6311a84bf20deef544
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-07-30 13:10:43 +1000

    create the README.md, main.h and the first function

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the program name or the first command-line argument.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+	char *name;
+
+	name = argv[0];
+
+	if (argc > 1)
+	{
+		name = argv[1];
+	}
+
+	printf("%s", name);
+
+	return (0);
+
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,4 @@
+#This directory was created in order to store the functions created for Holberton School Australia module - "C - argc, argv" 
+
+Function 1:
+Write a program that prints its name, followed by a new line. 
