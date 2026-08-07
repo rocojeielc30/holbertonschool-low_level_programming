ef9d8dfde685cf7db2d840644a223580a3f4636c
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-08-07 13:04:26 +1000

    add betty description

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * prime_helper - recursively checks whether a number is prime
+ * @n: the number being tested
+ * @i: the current candidate divisor being tested
+ *
+ * Return: 0 if n is not prime, 1 if n is prime
+ */
 int prime_helper(int n, int i)
 {
 	if (n <= 1)
@@ -8,7 +15,7 @@ int prime_helper(int n, int i)
 	}
 	if (i * i > n)
 	{
-		return (i);
+		return (1);
 	}
 	if (n % i == 0)
 	{
@@ -20,7 +27,13 @@ int prime_helper(int n, int i)
 
 }
 
+/**
+ * is_prime_number - determines if a number is a prime number
+ * @n: the number to check
+ *
+ * Return: 1 if n is prime, 0 if n is not prime
+ */
 int is_prime_number(int n)
 {
-	return prime_helper(n, 0);
+	return prime_helper(n, 2);
 }
