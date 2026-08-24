bd909aaca955c4ddb9e5e4e102aa2fcef22cb505
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-08-25 00:11:14 +1000

    updated main.h

--- file_io/main.h
+++ file_io/main.h
@@ -11,5 +11,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void close_fd(int fd);
+void open_files(char *file_from, char *file_to, int *fd_from, int *fd_to);
+void copy_content(int fd_from, int fd_to, char *file_from, char *file_to);
 
 #endif
